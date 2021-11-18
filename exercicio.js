d6759671738a2e518442e2c414b921b180142070
diogoestudante1 <diogo.informaticaweb@gmail.com>
2021-11-18 19:26:24 -0300

    ativividade

---
+++ exercicio.js
@@ -0,0 +1,84 @@
+[
+
+    {"id": 1,
+    "tenis": "torneio de tenis",
+     "local" { 
+      "nome": "aberto da australia"
+      "publico" : 5000,
+  },
+  
+  {"id": 2,
+    "jogadores"
+     "nome": Djokovic { 
+     "idade": 34,
+     "título" : true,
+     "país" : "Sérvia"
+  },
+  
+  {"id": 3,
+    "jogadores"
+     "nome": Nadal { 
+     "idade": 35,
+     "título" : true,
+     "país" : "Espanha"
+  },
+  
+  {"id": 4,
+    "jogadores"
+     "nome": Federer { 
+     "idade": 40,
+     "título" : true,
+     "país" : "Suíça"
+  },
+  
+  {"id": 5,
+    "jogadores"
+     "nome": Sampras 
+     "idade": 50,
+     "título" : true,
+     "país" : " EUA"
+  },
+  
+  {"id": 6,
+    "jogadores"
+     "nome": Guga  
+     "idade": 48,
+     "título" : true,
+     "país" : " Brasil"
+  },
+  
+  {"id": 7,
+    "jogadores"
+     "nome": Zverev 
+     "idade": 25,
+     "título" : true,
+     "país" : " Alemanhã"
+  },
+  
+  {"id": 8,
+    "jogadores"
+     "nome": Thiem 
+     "idade": 25,
+     "título" : true,
+     "país" : " Astria"
+  },
+  
+  {"id": 9,
+    "jogadores"
+     "nome": Del Potro,
+     "idade": 35,
+     "título" : true,
+     "país" : " Argentina"
+  },
+  
+  
+  {"id": 10,
+    "jogadores"
+     "nome": Agassi 
+     "idade": 51,
+     "título" : true,
+     "país" : " EUA"
+  }
+  ]
+  
+  
